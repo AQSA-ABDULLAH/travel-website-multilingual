0f2baf3e0c2982766b807249872cc05045bb890f
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-12-11 11:24:02 +0500

    add animation on hero section

--- components/hero/Main.tsx
+++ components/hero/Main.tsx
@@ -41,27 +41,25 @@ export default function Main() {
         {/* Left Section */}
         <div
           ref={leftSectionRef}
-          className={`w-full md:w-[55%] 2xl:w-[600px] lg:w-[55%] flex flex-col lg:items-start items-center text-center lg:text-left ${
-            isLeftVisible ? "animate-fadeRight" : "opacity-0"
-          }`}
+          className={`w-full md:w-[55%] 2xl:w-[600px] lg:w-[55%] flex flex-col lg:items-start items-center text-center lg:text-left`}
         >
           <span
             className={`text-[14px] sm:text-[22px] lg:text-[20px] 2xl:text-[24px] text-[#DF6951] uppercase font-semibold mb-8 ${
-              isLeftVisible ? "animate-fadeDown" : "opacity-0"
+              isLeftVisible ? "animate-fadeLeftToRight" : "opacity-0"
             }`}
           >
             {t("heading")}
           </span>
           <h1
             className={`text-[32px] px-20 sm:px-0 sm:text-[88px] lg:text-[96px] 2xl:text-[84px] text-center lg:text-left text-[#181E4B] font-volkhov font-black leading-tight sm:leading-loose 2xl:leading-large-loose desktop:leading-extra-loose mb-8 z-10 ${
-              isLeftVisible ? "animate-zoomInUp" : "opacity-0"
+              isLeftVisible ? "animate-fadeLeftToRight" : "opacity-0"
             }`}
           >
             {t("title")}
           </h1>
           <p
             className={`text-[12px] px-20 sm:px-0 sm:text-[20px] lg:text-[17px] 2xl:text-[16px] desktop:text-[22px] text-[#5E6282] w-[480px] 2xl:w-[600px] desktop:w-[800px] tracking-wider font-medium mb-6 ${
-              isLeftVisible ? "animate-fadeUp" : "opacity-0"
+              isLeftVisible ? "animate-fadeLeftToRight" : "opacity-0"
             }`}
           >
             {t("paragraph")}
@@ -73,9 +71,7 @@ export default function Main() {
             }`}
           >
             <button
-              className={`text-white hover:bg-orange-600 text-[18px] 2xl:text-[30px] bg-[#F1A501] px-6 py-4 rounded-lg sm:mr-4 ${
-                isLeftVisible ? "animate-jump" : "opacity-0"
-              }`}
+              className={`text-white hover:bg-orange-600 text-[18px] 2xl:text-[30px] bg-[#F1A501] px-6 py-4 rounded-lg sm:mr-4`}
             >
               {t("button1")}
             </button>
@@ -83,9 +79,7 @@ export default function Main() {
               <img
                 src="/assest/hero/Play Demo.png"
                 alt="Play Demo"
-                className={`w-[150px] sm:w-full 2xl:w-[300px] ${
-                  isLeftVisible ? "animate-fadeDownRight" : "opacity-0"
-                }`}
+                className={`w-[150px] sm:w-full 2xl:w-[300px]`}
               />
             </button>
           </div>
@@ -101,9 +95,7 @@ export default function Main() {
           <img
             src="/assest/hero/Image.png"
             alt="Travel"
-            className={`w-[300px] sm:w-full h-auto 2xl:w-[653.87px] 2xl:h-[786px] desktop:w-[700px] desktop:h-[880px] object-cover mt-12 lg:mt-2 ${
-              isRightVisible ? "animate-zoomInUp" : "opacity-0"
-            }`}
+            className={`w-[300px] sm:w-full h-auto 2xl:w-[653.87px] 2xl:h-[786px] desktop:w-[700px] desktop:h-[880px] object-cover mt-12 lg:mt-2`}
           />
         </div>
       </div>
